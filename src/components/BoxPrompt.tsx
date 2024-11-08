15e0d5bdb14c1b70c343fed7c06061357e3f3dbc
Aleksey Pi <aleksey.pilyugin@gmail.com>
2024-11-08 20:14:09 +0100

    Show SettingsDialog on corresponding button click

--- src/components/BoxPrompt.tsx
+++ src/components/BoxPrompt.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import { Autocomplete } from './Autocomplete';
 import { FaCog } from 'react-icons/fa';
+import { SettingsDialog } from './SettingsDialog'; // Make sure to import the SettingsDialog component
 
 export const BoxPrompt: React.FC = () => {
   const [prompt, setPrompt] = useState('');
+  const [isSettingsDialogOpen, setIsSettingsDialogOpen] = useState(false);
+
+  const handleSettingsClick = () => {
+    setIsSettingsDialogOpen(true);
+  };
+
+  const handleCloseSettingsDialog = () => {
+    setIsSettingsDialogOpen(false);
+  };
 
   return (
     <div id="promptBox">
@@ -13,10 +23,15 @@ export const BoxPrompt: React.FC = () => {
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
       />
-      <button id="settingsButton" style={{ position: 'absolute', top: 0, right: 0 }}>
+      <button
+        id="settingsButton"
+        style={{ position: 'absolute', top: 0, right: 0 }}
+        onClick={handleSettingsClick}
+      >
         <FaCog />
       </button>
       <Autocomplete inputText={prompt} onSelect={(text) => setPrompt(text)} />
+      {isSettingsDialogOpen && <SettingsDialog onUpdate={handleCloseSettingsDialog} />}
     </div>
   );
 };
