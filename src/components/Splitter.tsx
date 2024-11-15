18bddbc9ac229aaa2dc63b120430dac42e9d34bb
Aleksey Pi <aleksey.pilyugin@gmail.com>
2024-11-15 21:30:22 +0100

    Cleanup

--- src/components/Splitter.tsx
+++
@@ -1,6 +0,0 @@
-// src/components/Splitter.tsx
-import React from 'react';
-
-export const Splitter: React.FC = () => {
-  return <div className="splitter" style={{ height: '5px', cursor: 'row-resize' }} />;
-};
